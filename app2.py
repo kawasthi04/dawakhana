31fd1bf8b95e2f18eaa4af611de5534e252804f5
Kushagra Awasthi <65890170+kawasthi04@users.noreply.github.com>
2025-01-19 12:21:26 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -92,7 +92,7 @@ def login():
 
     col1, col2 = st.columns([1, 2])
     with col1:
-        st.image("static/pills_bottle_logo.svg", caption="The ultimate dispensary manager!", width=350)
+        st.image("pills_bottle_logo.svg", caption="The ultimate dispensary manager!", width=350)
     with col2:
         # Check if the user is in sign-up mode
         if 'sign_up_mode' not in st.session_state:
@@ -503,7 +503,7 @@ def view_orders():
 
 # Customer Dashboard
 def customer_dashboard():
-    st.sidebar.image("static/pills_bottle_logo.svg")
+    st.sidebar.image("pills_bottle_logo.svg")
     st.sidebar.header("Customer Menu")
     st.sidebar.button("Logout", on_click=logout)
 
